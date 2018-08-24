fbf93814469b5e0dd831729996a82e380fca3368
chamiamarillo <yamil.amarillo@unahur.edu.ar>
2018-08-24 18:53:52 -0300

    CLASE 24/08/2018 ejercicio con for

--- Ejercicio5confor.java
+++ Ejercicio5confor.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Ejercicio5{
+public class Ejercicio5confor{
   public static void main (String[] args){
     System.out.println("Ingrese un numero entre 1 y 100 para para sumarlos y mostrar por pantalla");
     // para leer por teclado
@@ -9,9 +9,8 @@ public class Ejercicio5{
     // hasta aca
     double resultado=0;
     int contador=0;
-    while (contador<valorIngresado){
-      contador++;
-      resultado=resultado+teclado.nextDouble(); // con esto teclado.nextDouble() directamente sumo lo que ingresa por teclado
+    for (contador=0;contador<valorIngresado;contador++){
+      resultado=resultado+teclado.nextDouble(); // con esto teclado.nextDouble() directamente sumo lo que ingresa por teclado   
     }
     System.out.println("La suma de de los numeros es " + resultado);
   }
---
+++ Ejercicio6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Ejercicio6{
+  public static void main (String[] args){
+    System.out.println("Ingrese n numero reales y cuando coloque un 0 mostrar la suma por pantalla");
+    // para leer por teclado
+    Scanner teclado=new Scanner(System.in);
+    double valorIngresado=teclado.nextDouble();
+    // hasta aca
+    double resultado=valorIngresado; // ya empiezo almacenar el valor de teclado
+    for (;valorIngresado!=0;){
+      valorIngresado=teclado.nextDouble();
+      resultado=resultado+valorIngresado; // para poder salir del for la variable valorIngresado almacena lo que carga en teclado   
+    }
+    System.out.println("La suma de de los numeros es " + resultado);
+  }
+}
